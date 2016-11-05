609de396cb9c406993bf4ddf8489a428f9f35e94
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-11-05 00:28:46 -0200

    Wrote missing functions in main
    Also added levels of indentation to output

--- src/main.c
+++ src/main.c
@@ -38,23 +38,75 @@ void insert_values(char *filename) {
   }
 }
 
-void query_values(char *filename) { printf("Querying from %s\n", filename); }
+void query_values(char *filename) {
+  FILE *file;
+  int value;
+  printf("Querying from %s\n", filename);
+  file = fopen(filename, "r");
+  if (file) {
+    bst_report = report_create();
+    report_clock_start(&bst_report);
+    while (fscanf(file, "%d", &value) != EOF) {
+      BST_get(&bst_report, bst_root, value);
+    }
+    report_clock_stop(&bst_report);
+    fclose(file); /* Close the stream before updating the report. */
+    BST_update_report(&bst_report, bst_root);
+  }
+  file = fopen(filename, "r");
+  if (file) {
+    avl_report = report_create();
+    report_clock_start(&avl_report);
+    while (fscanf(file, "%d", &value) != EOF) {
+      AVL_get(&avl_report, avl_root, value);
+    }
+    report_clock_stop(&avl_report);
+    fclose(file); /* Close the stream before updating the report. */
+    AVL_update_report(&avl_report, avl_root);
+  }
+}
 
-void remove_values(char *filename) { printf("Removing from %s\n", filename); }
+void remove_values(char *filename) {
+  FILE *file;
+  int value;
+  printf("Removing from %s\n", filename);
+  file = fopen(filename, "r");
+  if (file) {
+    bst_report = report_create();
+    report_clock_start(&bst_report);
+    while (fscanf(file, "%d", &value) != EOF) {
+      BST_remove(&bst_report, &bst_root, value);
+    }
+    report_clock_stop(&bst_report);
+    fclose(file); /* Close the stream before updating the report. */
+    BST_update_report(&bst_report, bst_root);
+  }
+  file = fopen(filename, "r");
+  if (file) {
+    avl_report = report_create();
+    report_clock_start(&avl_report);
+    while (fscanf(file, "%d", &value) != EOF) {
+      AVL_remove(&avl_report, &avl_root, value);
+    }
+    report_clock_stop(&avl_report);
+    fclose(file); /* Close the stream before updating the report. */
+    AVL_update_report(&avl_report, avl_root);
+  }
+}
 
 static void print_report(Report report) {
-  printf("Time: %ld ms\n", report.time);
-  printf("Nodes: %ld\n", report.nodes);
-  printf("Height: %ld\n", report.height);
-  printf("Factor: %ld\n", report.factor);
-  printf("Comparisons: %ld\n", report.comparisons);
-  printf("Rotations: %ld\n", report.rotations);
+  printf("  Time: %ld ms\n", report.time);
+  printf("  Nodes: %ld\n", report.nodes);
+  printf("  Height: %ld\n", report.height);
+  printf("  Factor: %ld\n", report.factor);
+  printf("  Comparisons: %ld\n", report.comparisons);
+  printf("  Rotations: %ld\n", report.rotations);
 }
 
 void statistics(void) {
-  printf("Statistics for BST:\n");
+  printf(" Statistics for BST:\n");
   print_report(bst_report);
-  printf("Statistics for AVL:\n");
+  printf(" Statistics for AVL:\n");
   print_report(avl_report);
 }
 
@@ -69,7 +121,11 @@ int main(int argc, char **argv) {
   bst_report = report_create();
   BST_initialize(&bst_root);
   AVL_initialize(&avl_root);
-  insert_values("input/n1000.txt");
+  insert_values("input/n100000.txt");
+  statistics();
+  query_values("input/n10000.txt");
+  statistics();
+  remove_values("input/n1000.txt");
   statistics();
   if (argc < 2) {
     printf("Pass a filename to the program.\n");
