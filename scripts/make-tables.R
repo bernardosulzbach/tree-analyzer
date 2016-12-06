3783bd20d54be4d2ed856cf99d3c51698a642758
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-12-06 19:38:47 -0200

    Finished typesetting the tables

--- scripts/make-tables.R
+++ scripts/make-tables.R
@@ -1,9 +1,13 @@
 #!/usr/bin/env Rscript
 
 library(xtable)
-options(xtable.floating=FALSE)
 options(xtable.timestamp="")
 options(xtable.include.rownames=FALSE)
+options(xtable.table.placement="H")
+
+toString <- function(vec) {
+    vec <- replace(format(vec, big.mark=",", scientific=FALSE), is.na(vec), "-")
+}
 
 bakeResults <- function(filename, prefix="") {
     data <- read.csv(file=filename, sep=",", head=TRUE)
@@ -16,6 +20,8 @@ bakeResults <- function(filename, prefix="") {
         label <- gsub("\\.", " ", label)
         texName <- tolower(paste(prefix, gsub(" ", "-", label), ".tex", sep=""))
 
+        alignment <- "|r|r|r|r|"
+
         # Add the time unit if needed.
         label <- gsub("(Insertion|Query|Removal) ", "", label)
         if (label == "Duration") {
@@ -23,23 +29,26 @@ bakeResults <- function(filename, prefix="") {
             # Also change zeros to NAs.
             table[table == 0] <- NA
         }
-        # Wait after changing zero times to NAs to calculate factors.
-        table <- data.frame(table, table[,2] / table[,3])
-        # Nothing here is Inf, it should be NA.
-        table[table == Inf] <- NA
-        
+        # Do not calculate ratios in factors.
+        if (label != "Factor") {
+            # Wait after changing zero times to NAs to calculate factors.
+            table <- data.frame(table, table[,2] / table[,3])
+            table[4][table[4] == 0] <- NA
+            colnames(table)[4] <- "Ratio"
+            alignment <- "|r|r|r|r|r|"
+        }
         colnames(table)[2] <- paste("BST", label)
         colnames(table)[3] <- paste("AVL", label)
-        colnames(table)[4] <- "Ratio"
+        # Nothing here is Inf, it should be NA.
+        table[table == Inf] <- NA
 
-        table[1] <- format(table[1], big.mark=",", scientific=FALSE)
-        table[2] <- format(table[2], big.mark=",", scientific=FALSE)
-        table[3] <- format(table[3], big.mark=",", scientific=FALSE)
+        table[1] <- toString(table[1])
+        table[2] <- toString(table[2])
+        table[3] <- toString(table[3])
 
-        xtab <- xtable(table, latex.environments = "center")
-        display(xtab) <- c('d', 'd', 's', 's', 'f')
-        align(xtab) <- "rrrrr"
-        print(xtab, file=texName, tabular.environment="tabularx", width="\\textwidth")
+        xtab <- xtable(table, latex.environments="center", align=alignment)
+        print(xtab, file=texName, NA.string="-")
+        # print(xtab, file=texName, tabular.environment="tabularx", NA.string="-")
     }
 
     for (i in c(2, 3, 4, 5, 10, 11, 12, 13, 18, 19, 20, 21)) {
